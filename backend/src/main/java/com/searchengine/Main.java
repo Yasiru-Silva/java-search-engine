e331a026bf5021da265b5c57effc1380f93631fa
Yasiru Silva <yasiru017@gmail.com>
2026-06-10 09:42:52 +0530

    feat(indexer): implement InvertedIndex with addWord and search methods

--- backend/src/main/java/com/searchengine/Main.java
+++ backend/src/main/java/com/searchengine/Main.java
@@ -2,7 +2,7 @@ package com.searchengine;
 
 import com.searchengine.trie.Trie;
 import java.util.List;
-
+import com.searchengine.indexer.InvertedIndex;
 public class Main {
 
     public static void main(String[] args) {
@@ -28,5 +28,21 @@ public class Main {
         System.out.println("\nAutocomplete 'ca': " + trie.autocomplete("ca"));
         System.out.println("Autocomplete 'jav': " + trie.autocomplete("jav"));
         System.out.println("Autocomplete 'xyz': " + trie.autocomplete("xyz"));
+
+        // Create an inverted index
+        InvertedIndex invertedIndex = new InvertedIndex();
+
+// Add some words and which files they appear in
+        invertedIndex.addWord("java", "doc1.txt");
+        invertedIndex.addWord("java", "doc2.txt");
+        invertedIndex.addWord("programming", "doc1.txt");
+        invertedIndex.addWord("programming", "doc3.txt");
+        invertedIndex.addWord("python", "doc3.txt");
+
+// Test search
+        System.out.println("\nFiles containing 'java': " + invertedIndex.search("java"));
+        System.out.println("Files containing 'programming': " + invertedIndex.search("programming"));
+        System.out.println("Files containing 'ruby': " + invertedIndex.search("ruby"));
+
     }
 }
